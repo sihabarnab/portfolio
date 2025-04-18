aa16075d5d35e0bbf003b8fff2ce695800f6ee3c
Sihab Hossain <sihabarnab123@gmail.com>
2025-04-18 12:54:50 +0600

    Add 3D models and Lights

---
+++ src/components/HeroModels/HeroExperience.jsx
@@ -0,0 +1,33 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
+
+const HeroExperience = () => {
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px" });
+
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <OrbitControls
+        enablePan={false} // Prevents panning of the scene
+        enableZoom={!isTablet} // Disables zoom on tablets
+        maxDistance={20} // Maximum distance for zooming out
+        minDistance={5} // Minimum distance for zooming in
+        minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
+        maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
+      />
+      <HeroLights />
+      <group
+        scale={isMobile ? 0.7 : 1} // Adjust scale for mobile devices
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
+    </Canvas>
+  );
+};
+
+export default HeroExperience;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,24 +1,59 @@
-import React from "react";
+import Button from "../components/button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { words } from "../constants/index.js";
 
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
-        <img src="/image/bg.png" alt="background" />
+        <img src="/images/bg.png" alt="background" />
       </div>
 
       <div className="hero-layout">
         {/* LEFT: HERO CONTENT  */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-            <div className="flex flex-col gap-7">
-                <div className="hero-text">
-                    <h1>Shaping Designs</h1>
-                    <h1>into Real Projects</h1>
-                    <h1>that Deliver Results</h1>
-                </div>
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span> {word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
             </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none ">
+              Hi, I'm Sihab, a developer based in Dhaka, Bangladesh with a
+              passion for code.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my work"
+            />
+          </div>
         </header>
-        {/* RIGHT: HERO CONTENT  */}
+        {/* RIGHT: 3D MODEL  */}
+        <figure>
+          <div className="hero-3d-layout ">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
